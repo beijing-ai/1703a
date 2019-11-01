c52b2279d30915e4f24eaeb0cb8f14d74e4c8535
gaobo25 <48475596+gaobo25@users.noreply.github.com>
2019-11-01 19:48:27 +0800

    2019年11月1日

---
+++ 2019-11-1/pro1zuoye.cpp
@@ -0,0 +1,106 @@
+﻿// pro1zuoye.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//有3个整数a, b, c，由键盘输入，输出其中最大的数。
+
+	//求自然数1~10之和
+
+	/*int a=0;
+	int x;
+	for (x = 0; x <= 10; x++)
+	{
+		a = a + x;
+	}
+	cout << a;*/
+
+
+	//输入一个整数，将各位数字反转后输出。
+	/*int n;
+	int m;
+	cout << "输入一个数：";
+	cin >> n;
+	cout << "翻转后的数为";
+	do
+	{
+		m = n%10;
+		cout << m;
+		n /= 10;
+	} while (n != 0);
+	cout << endl;*/
+
+
+
+	//输入一个整数，求出它的所有因子。
+
+	/*int a,i;
+	cout << "请输入一个整数";
+	cin >> a;
+	for (i = 1; i <= a; i++)
+	{
+		if (a%i==0)
+		{
+			cout << i;
+		}
+		cout << "\t";
+	}*/
+
+
+	//求两自然数m, n的最大公约数
+	/*int m, n;
+	cout << "请输入第一个数：";
+	cin >> m;
+	cout << "请输入第二个数：";
+	cin >> n;
+
+	int k,a;
+	for( k=1 ; k<=m && k<=n;k++)
+	{
+		if (m%k == 0 && n%k == 0)
+		{
+			a = k;
+		}
+		
+	}
+	cout <<"最大公因数：" << a;*/
+
+
+	//求1 - 100以内的素数
+	int i, j;
+	bool flag;
+	for (i = 2; i < 101; i++) {
+		flag = true;
+		for (j = 2; j < i; j++)
+		{
+			if (i%j == 0)
+			{
+				flag = false;
+			}
+		}
+		if (flag)
+		{
+			cout << i << "\n";
+		}
+	}
+	return 0;
+
+	//鸡兔共有30只，脚共有90只，问鸡兔各有多少
+	/*int x, y;
+	for (x = 1; x <= 30; x++)
+	{
+		y = 30 - x;
+		if ((2 * x) + (4 * y) == 90)
+		{
+			cout << "鸡" << x <<"\t" ;
+			cout << "兔子" << y;
+
+		}
+		
+	}*/
+}
+
---
+++ 2019-11-1/pro2zuoye.cpp
@@ -0,0 +1,38 @@
+﻿// pro2zuoye.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*char senteance[100], prev = ' ';
+	int i, num = 0;
+	cout << "请输入句子,以空格分隔：";
+
+	gets_s(senteance);
+	for (i = 0; senteance[i] != '\0'; i++) {
+		if (prev == ' '&& senteance[i] != ' ')++num;
+		prev = senteance[i];
+	}
+	cout << "单词个数：" << num << endl;*/
+
+	int a;
+	enum light{red=1,yello,green};
+	cout << "请输入信号灯编号（1,2,3）：";
+	cin >> a;
+	switch (a)
+	{
+	case red :
+		cout << "stop"; break;
+	case yello:
+		cout << "wait"; break;
+	case green:
+		cout << "go"; break;
+	default:
+		break;
+	}
+
+}
+

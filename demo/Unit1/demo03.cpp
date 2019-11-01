9e0f8b6745c589604d093b711490a126e2b7e765
wangzhao <1152403627@qq.com>
2019-11-01 20:14:26 +0800

    11月1日作业

---
+++ demo/Unit1/demo03.cpp
@@ -0,0 +1,24 @@
+﻿// demo03.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//输入一个整数，将各位数字反转后输出。
+	int n;
+	cin >> n;
+	int num = 0;
+	while (1)
+	{
+		num = n % 10;//取余
+		n /= 10;//取整
+		cout << num;
+		if (n < 1)
+		{
+			break;
+		}
+	}
+}
---
+++ demo/Unit1/demo07.cpp
@@ -0,0 +1,28 @@
+﻿// demo07.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //鸡兔共有30只，脚共有90只，问鸡兔各有多少？
+	int j,t,num=30,jiao=88;
+	for (j=1;j<num;j++) 
+	{
+		for (t=1;t<num;t++) 
+		{
+			if (j + t == num) 
+			{
+				if (2*j+4*t==jiao) 
+				{
+					cout << j << endl;
+					cout << t;
+				}
+			}
+		}
+	}
+
+}
+

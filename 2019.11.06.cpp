f94efe98d3ede05b496461b78ee78bca68d3e9a6
wuzhiyang <448556244@qq.com>
2019-11-06 21:03:09 +0800

    2019-11-06

---
+++ 2019.11.06.cpp
@@ -0,0 +1,69 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+	protected:
+		int width, height;
+
+	public:
+		Shape(int a=0,int b=0)
+		{
+			width = a;
+			height = b;
+		}
+		~Shape()
+		{
+		}
+
+		int area() {
+			cout << "Parent class area:" << endl;
+			return 0;
+		}
+};
+
+
+class Rectangle:public Shape
+{
+	public:
+		Rectangle(int a = 0, int b = 0) :Shape(a, b)
+		{
+		};
+		~Rectangle() 
+		{
+		};
+		
+		int area() 
+		{
+			cout << "Rectangle class area:" << endl;
+			return (width * height);
+		}
+};
+
+class Triangle :public Shape {
+	public:
+		Triangle(int a = 0, int b = 0) :Shape(a, b)
+		{
+
+		}
+
+		int area() 
+		{
+			cout << "Triangle class area:" << endl;
+			return (width * height / 2);
+		}
+
+};
+
+
+int main()
+{
+	Rectangle r1(10, 5);
+	cout <<"矩形的面积是：" << r1.area() << endl;
+
+	Triangle t1(10, 6);
+	cout << "三角形的面积是：" << t1.area();
+}
+
+
